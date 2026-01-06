b62472af3b69f0a724d575a8df14321f4ea7ea4e
Arshad Ansari <aarshadansariabc@gmail.com>
2026-01-06 12:41:50 +0530

    add some basic funtion

--- day 13/higher_Lower.py
+++ day 13/higher_Lower.py
@@ -1,1 +1,11 @@
-form Game_data import data
+import random
+from Game_data import data
+
+random_a = random.choice(data)
+random_b = random.choice(data)
+
+if random_a == random_b:
+    random_b = random.choice(data)
+
+account_name = random_a['name']
+print(account_name)
